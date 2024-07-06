aad04ef9fefd402e762b791c9f952537e5faa4c8
Eddy Jaga <eddyjaga@gmail.com>
2024-07-06 21:24:39 +1000

    Initial project

---
+++ README.md
@@ -0,0 +1,53 @@
+# Quote Editor Turbo Rails Project
+
+## Description
+
+Just fun and learn more about Rails 7, Turbo, Stimulus, and Hotwire. You can fork this app and develop more if you want to.
+
+## System Requirements
+
+- Ruby 3.3.0
+- Node 18.7.0
+- Rails 7.1.3
+
+## Installation
+
+1. Clone the repository:
+
+    ```sh
+    git clone https://github.com/eddyjaga/quote-editor.git
+    cd quote-editor
+    ```
+
+2. Install dependencies:
+
+    ```sh
+    bundle install
+    yarn install
+    ```
+
+3. Set up the database:
+
+    ```sh
+    rails db:setup
+    ```
+
+4. Start the Rails server:
+
+    ```sh
+    rails server
+    ```
+
+## Usage
+
+1. Open your browser and navigate to `http://localhost:3000`.
+
+2. Explore the app and start editing quotes.
+
+## Contributing
+
+Feel free to fork this repository and submit pull requests. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ test/system/quotes_test.rb
@@ -0,0 +1,52 @@
+require "application_system_test_case"
+
+class QuotesTest < ApplicationSystemTestCase
+
+  setup do
+    @quote = quotes(:first)
+  end
+
+  test "showing a quote" do
+    visit quotes_url
+
+    click_link @quote.name
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "updating a quote" do
+    visit quotes_url
+    assert_selector "h1", text: "Quotes"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Editing Quote"
+
+    fill_in "Name", with: "Updated quote"
+    click_on "Update Quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated quote"
+  end
+
+  test "destroying a quote" do
+    visit quotes_url
+    assert_text @quote.name
+
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
+  end
+
+  test "visiting the index" do
+    visit quotes_url
+  
+    assert_selector "h1", text: "Quotes"
+
+    click_on "New Quote"
+    assert_selector "h1", text: "New Quote"
+
+    fill_in "Name", with: "Capybara quote"
+    click_on "Create Quote"
+
+    asset_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
+  end
+end
